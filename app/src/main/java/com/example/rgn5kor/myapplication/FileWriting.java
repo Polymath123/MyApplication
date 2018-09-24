8a1454ec847770e8f758a2971866d7df10c6d3ba
RGN5KOR <ploymath123@gmail.com>
2018-09-24 14:21:40 +0530

    Removed Unwanted code

---
+++ app/src/main/java/com/example/rgn5kor/myapplication/FileWriting.java
@@ -0,0 +1,31 @@
+package com.example.rgn5kor.myapplication;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileWriting extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
